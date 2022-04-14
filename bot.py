1ad63ebeac9fdc3953e1c81d1408a1beedd25903
Maxim Zavalniuk <mezgoodle@gmail.com>
2022-04-14 09:57:13 +0300

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,9 +1,7 @@
-import asyncio
 import logging
 
 from aiogram import Bot, Dispatcher
 from aiogram.contrib.fsm_storage.memory import MemoryStorage
-from aiogram.contrib.fsm_storage.redis import RedisStorage2
 
 from tgbot.config import load_config
 from tgbot.filters.admin import AdminFilter
@@ -13,37 +11,35 @@ from tgbot.handlers.user import register_user
 from tgbot.middlewares.db import DbMiddleware
 
 logger = logging.getLogger(__name__)
+config = load_config()
+storage = MemoryStorage()
+bot = Bot(token=config.tg_bot.token, parse_mode='HTML')
+dp = Dispatcher(bot, storage=storage)
+bot['config'] = config
 
 
-def register_all_middlewares(dp):
-    dp.setup_middleware(DbMiddleware())
+def register_all_middlewares(dispatcher: Dispatcher):
+    logger.info('Registering middlewares')
+    dispatcher.setup_middleware(DbMiddleware())
 
 
-def register_all_filters(dp):
-    dp.filters_factory.bind(AdminFilter)
+def register_all_filters(dispatcher: Dispatcher):
+    logger.info('Registering filters')
+    dispatcher.filters_factory.bind(AdminFilter)
 
 
-def register_all_handlers(dp):
-    register_admin(dp)
-    register_user(dp)
+def register_all_handlers(dispatcher: Dispatcher):
+    logger.info('Registering handlers')
+    register_admin(dispatcher)
+    register_user(dispatcher)
+    register_echo(dispatcher)
 
-    register_echo(dp)
 
-
-async def main():
+if __name__ == '__main__':
     logging.basicConfig(
         level=logging.INFO,
         format=u'%(filename)s:%(lineno)d #%(levelname)-8s [%(asctime)s] - %(name)s - %(message)s',
     )
-    logger.info("Starting bot")
-    config = load_config(".env")
-
-    storage = RedisStorage2() if config.tg_bot.use_redis else MemoryStorage()
-    bot = Bot(token=config.tg_bot.token, parse_mode='HTML')
-    dp = Dispatcher(bot, storage=storage)
-
-    bot['config'] = config
-
     register_all_middlewares(dp)
     register_all_filters(dp)
     register_all_handlers(dp)
@@ -51,14 +47,8 @@ async def main():
     # start
     try:
         await dp.start_polling()
+        logger.info("Starting bot")
     finally:
         await dp.storage.close()
         await dp.storage.wait_closed()
-        await bot.session.close()
-
-
-if __name__ == '__main__':
-    try:
-        asyncio.run(main())
-    except (KeyboardInterrupt, SystemExit):
-        logger.error("Bot stopped!")
+        logger.info("Bot stopped")
