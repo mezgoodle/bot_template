d930ecf53aa841d79d0c24934e7f9093253579f2
Maxim Zavalniuk <mezgoodle@gmail.com>
2022-04-14 10:14:42 +0300

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,10 +1,10 @@
 import logging
 
-from aiogram import Bot, Dispatcher
+from aiogram import Bot, Dispatcher, executor
 from aiogram.contrib.fsm_storage.memory import MemoryStorage
 
 from tgbot.config import load_config
-from tgbot.filters.admin import AdminFilter
+from tgbot.filters.admin import IsAdminFilter
 from tgbot.handlers.admin import register_admin
 from tgbot.handlers.echo import register_echo
 from tgbot.handlers.user import register_user
@@ -25,7 +25,7 @@ def register_all_middlewares(dispatcher: Dispatcher):
 
 def register_all_filters(dispatcher: Dispatcher):
     logger.info('Registering filters')
-    dispatcher.filters_factory.bind(AdminFilter)
+    dispatcher.filters_factory.bind(IsAdminFilter)
 
 
 def register_all_handlers(dispatcher: Dispatcher):
@@ -35,20 +35,30 @@ def register_all_handlers(dispatcher: Dispatcher):
     register_echo(dispatcher)
 
 
+async def on_startup(dispatcher: Dispatcher):
+    register_all_middlewares(dispatcher)
+    register_all_filters(dispatcher)
+    register_all_handlers(dispatcher)
+    logger.info('Bot started')
+
+
+async def on_shutdown(dispatcher: Dispatcher):
+    await dispatcher.storage.close()
+    await dispatcher.storage.wait_closed()
+    logger.info('Bot shutdown')
+
+
 if __name__ == '__main__':
     logging.basicConfig(
         level=logging.INFO,
         format=u'%(filename)s:%(lineno)d #%(levelname)-8s [%(asctime)s] - %(name)s - %(message)s',
     )
-    register_all_middlewares(dp)
-    register_all_filters(dp)
-    register_all_handlers(dp)
-
-    # start
     try:
-        await dp.start_polling()
-        logger.info("Starting bot")
-    finally:
-        await dp.storage.close()
-        await dp.storage.wait_closed()
-        logger.info("Bot stopped")
+        executor.start_polling(
+            dp,
+            skip_updates=True,
+            on_startup=on_startup,
+            on_shutdown=on_shutdown,
+        )
+    except KeyboardInterrupt:
+        logger.info("Stopping bot")
