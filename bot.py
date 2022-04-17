a101a3147e4140e522268e34f926d96196075ed0
Maxim Zavalniuk <mezgoodle@gmail.com>
2022-04-17 18:59:27 +0300

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,7 +1,11 @@
+import functools
 import logging
+import os
 
-from aiogram import Dispatcher, executor
+from aiogram import Dispatcher
+from aiogram.utils.executor import start_polling, start_webhook
 
+from tgbot.config import load_config
 from tgbot.filters.admin import IsAdminFilter
 from tgbot.middlewares.throttling import ThrottlingMiddleware
 from tgbot.services.setting_commands import set_default_commands
@@ -30,11 +34,20 @@ async def register_all_commands(dispatcher: Dispatcher) -> None:
     await set_default_commands(dispatcher.bot)
 
 
-async def on_startup(dispatcher: Dispatcher) -> None:
+async def on_startup(dispatcher: Dispatcher, webhook_url: str = None) -> None:
     register_all_middlewares(dispatcher)
     register_all_filters(dispatcher)
     register_all_handlers(dispatcher)
     await register_all_commands(dispatcher)
+    # Get current webhook status
+    webhook = await dispatcher.bot.get_webhook_info()
+
+    if webhook_url:
+        await dispatcher.bot.set_webhook(webhook_url)
+        logger.info('Webhook was set')
+    elif webhook.url:
+        await dispatcher.bot.delete_webhook()
+        logger.info('Webhook was deleted')
     logger.info('Bot started')
 
 
@@ -49,12 +62,30 @@ if __name__ == '__main__':
         level=logging.INFO,
         format=u'%(filename)s:%(lineno)d #%(levelname)-8s [%(asctime)s] - %(name)s - %(message)s',
     )
-    try:
-        executor.start_polling(
-            dp,
-            skip_updates=True,
-            on_startup=on_startup,
-            on_shutdown=on_shutdown,
-        )
-    except KeyboardInterrupt:
-        logger.info("Stopping bot")
+    config = load_config()
+
+    # Webhook settings
+    HEROKU_APP_NAME = os.getenv('HEROKU_APP_NAME')
+    WEBHOOK_HOST = f'https://{HEROKU_APP_NAME}.herokuapp.com'
+    WEBHOOK_PATH = f'/webhook/{config.tg_bot.token}'
+    WEBHOOK_URL = f'{WEBHOOK_HOST}{WEBHOOK_PATH}'
+    # Webserver settings
+    WEBAPP_HOST = '0.0.0.0'
+    WEBAPP_PORT = int(os.getenv('PORT', 5000))
+
+    start_polling(
+        dispatcher=dp,
+        on_startup=on_startup,
+        on_shutdown=on_shutdown,
+        skip_updates=True,
+    )
+
+    # start_webhook(
+    #     dispatcher=dp,
+    #     on_startup=functools.partial(on_startup, webhook_url=WEBHOOK_URL),
+    #     on_shutdown=on_shutdown,
+    #     webhook_path=WEBHOOK_PATH,
+    #     skip_updates=True,
+    #     host=WEBAPP_HOST,
+    #     port=WEBAPP_PORT
+    # )
